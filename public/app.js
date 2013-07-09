d33cba8ceaadf23b3d33ac2fc35f4c850a050f36
Rashad Russell <rashadr@yahoo-inc.com>
2013-07-08 17:44:20 -0700

    Added drag and drop environment

--- public/app.js
+++ public/app.js
@@ -1,9 +1,9 @@
-YUI().use('node', 'view', function(Y) {
+YUI().use('node', 'view', 'dd-constrain', 'dd-proxy', 'dd-drop', function(Y) {
 
 	var LayoutGenerator, GeneralSettings;
 
 	// GeneralSettings View
-	// Responsible for settings that span the entire HTML DOM such as background-color, font-color, font-size, etc.
+	// Responsible for setting styles that will affect the entire HTML DOM such as background-color, font-color, font-size, etc.
 	GeneralSettings = Y.GeneralSettings = Y.Base.create('generalSettings', Y.View, [], {
 
 		template: Y.one('#general-settings').getHTML(),
@@ -11,7 +11,7 @@ YUI().use('node', 'view', function(Y) {
 		events: {
 			'.layout-selector': {change: 'changeLayout'},
 			'.font-size-selector': {change: 'changeFontSize'},
-			'.font-family-selector': {change: 'changeFontFamily'}
+			'.font-family-selector': {change: 'changeFontFamily'},
 		},
 
 		// ---- Event Handlers -------------------------------------------------------------------------------------
@@ -25,10 +25,12 @@ YUI().use('node', 'view', function(Y) {
 				Y.one('#layout-iframe').setAttribute('src', path);
 
 
-				Y.one('iframe').after('load', function() {
+				Y.one('iframe').after('load', function(e) {
+					e.stopPropagation();
 					iframeDocument = Y.one('#layout-iframe').get('contentWindow').get('document');
 					iframeHeight = iframeDocument.get('height');
 					Y.one('iframe').setStyles({'backgroundColor': '#fff','height': iframeHeight});
+					new DDDOM().render();
 				});
 
 		},
@@ -47,6 +49,7 @@ YUI().use('node', 'view', function(Y) {
 				iframeBody.setStyle('fontFamily', fontFamily);
 		},
 
+		// ---- Render View to DOM --------------------------------------------------------------------------------
 		render: function() {			
 			var container = this.get('container');
 
@@ -65,5 +68,87 @@ YUI().use('node', 'view', function(Y) {
 	});
 	new GeneralSettings().render();
 
+
+	// DDDOM View
+	// Responsible for establishing a drag & drop environment for DOM elements in the layout
+	DDDOM = Y.DDDOM = Y.Base.create('ddDOM', Y.View, [], {
+
+		// ---- Event Handlers -------------------------------------------------------------------------------------
+		dragProxy: function(e) {
+			// Get our drag object
+    		var drag = e.target;
+    		// Set some styles here
+    		drag.get('node').setStyle('opacity', '.25');
+    		drag.get('dragNode').set('innerHTML', drag.get('node').get('innerHTML'));
+    		drag.get('dragNode').setStyles({
+        		opacity: '.5',
+       			borderColor: drag.get('node').getStyle('borderColor'),
+       		 	backgroundColor: drag.get('node').getStyle('backgroundColor')
+    		});
+		},
+
+		postProxyStyle: function(e) {
+			var drag = e.target;
+    		// Put our styles back
+    		drag.get('node').setStyles({
+       			visibility: '',
+        		opacity: '1'
+    		});
+		},
+
+		adjustShim: function(e) {
+			// Get a reference to our drag and drop nodes
+    		var drag = e.drag.get('node'),
+        	drop = e.drop.get('node');
+    
+        	// Add the node to this list
+       	 	Y.DD.DDM.swapPosition(drag, drop);
+        	// Resize this nodes shim, so we can drop on it later.
+        	e.drop.sizeShim();
+    		
+		},
+
+		// ---- Render View to DOM --------------------------------------------------------------------------------
+		render: function() {			
+			var container = this.get('container');
+
+				Y.one(container).all('*').each(function(n) { 
+
+					if(n.hasClass('pure-edit')) {
+						var drag, drop;	
+
+						drag = new Y.DD.Drag({
+							node: n
+						}).plug(Y.Plugin.DDProxy, {
+							moveOnEnd: false
+						});
+
+						drop = new Y.DD.Drop({
+							node: n.get('parentNode')
+						});
+						
+						n.on('mouseover', function(e) {
+							e.target.setStyle('cursor', 'move');
+						});
+					}
+
+				});
+
+				// ---- Events -------------------------------------------------------------------------
+				Y.DD.DDM.on('drag:start', this.dragProxy);
+				Y.DD.DDM.on('drag:end'  , this.postProxyStyle);
+				Y.DD.DDM.on('drop:over' , this.adjustShim);
+
+			return this;
+		},
+
+	}, {
+		ATTRS: {
+			container: {
+				valueFn: function() { return Y.one('#layout-iframe').get('contentWindow').get('document').get('body'); }
+			}			
+		}
+	});
+	
 
 });
